57b36e2b29e37927afffe4a85ee4ef47475a7de0
Eno Isallari <enoisallari18@gmail.com>
2021-10-04 15:12:52 +0200

    commited project

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+<<<<<<< HEAD
+import {HomePageContainer} from '../homepage/homepage.styles'
+
+const HomePage = () => (
+  <div className='homepage'>
+    <HomePageContainer>
+  <Directory />
+  </HomePageContainer>
+=======
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+>>>>>>> fd9790f25ed32d418cc7e7809a2fef7b68a69df4
+  </div>
+);
+
+export default HomePage;
